2676ef32ba455d17b969e659bfe79a2833412df6
Hyunsoo Lee <29248842+hansLee-it@users.noreply.github.com>
2024-08-28 21:49:51 +0900

    Time: 0 ms (100%), Space: 44.9 MB (31.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=1;i<nums.length;i++){
             for(int j=i;j<nums.length;j++){
-                if(nums[i]+nums[j-1] == target){
-                    return new int[]{i,j};
+                if(nums[j]+nums[j-i] == target){
+                    return new int[]{j,j-i};
                 }
             }
         }
