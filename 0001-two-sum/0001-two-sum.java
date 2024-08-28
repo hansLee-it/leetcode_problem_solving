558a5267884eb31e82ed7d1a9046d3ef53ef35b3
Hyunsoo Lee <29248842+hansLee-it@users.noreply.github.com>
2024-08-28 21:45:35 +0900

    Time: 45 ms (29.9%), Space: 45 MB (31.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,8 +3,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j] == target){
-                    int[] result = {i,j};
-                    return result;
+                    return new int[]{i,j};
                 }
             }
         }
