771c053acd0be381831294c4c6cccda99751c37b
Hyunsoo Lee <29248842+hansLee-it@users.noreply.github.com>
2025-01-22 16:59:20 -0500

    Time: 16 ms (16.6%), Space: 45.1 MB (14.13%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -3,23 +3,17 @@ class Solution {
         if(n == 1) return "1";
         String recResult = countAndSay(n-1);
         String result = "";
-        char base = recResult.charAt(0);
-        int loopTime = 0;
-        int count = 0;
-        for(char c : recResult.toCharArray()){
-            if(base != c){ 
+        int count = 1;
+        for(int i = 1; i < recResult.length(); i++){
+            if(recResult.charAt(i-1) != recResult.charAt(i)){ 
                 // make count and base into String and add to the result && reset count
-                result += String.valueOf(count) + String.valueOf(base);
-                base = c;
+                result += String.valueOf(count) + String.valueOf(recResult.charAt(i-1));
                 count = 1;
             }else{ // if (base == c) rise count;
                 count++;
             }
-            loopTime++;
-            if(loopTime == recResult.length())
-                result += String.valueOf(count) + String.valueOf(base);
         }
-
+        result += String.valueOf(count) + String.valueOf(recResult.charAt(recResult.length()-1));
         return result;
     }
 }
