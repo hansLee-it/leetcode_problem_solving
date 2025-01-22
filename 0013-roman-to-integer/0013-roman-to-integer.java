8975768b38b30d74d711be96bc5685ab5a46bfd2
Hyunsoo Lee <29248842+hansLee-it@users.noreply.github.com>
2025-01-22 15:14:24 -0500

    Time: 2 ms (100%), Space: 44.9 MB (52.3%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = romanCharToInt(s.charAt(s.length()-1));
+
+        for(int i = s.length()-1 ; i>0 ; i--){
+            int afterChar = romanCharToInt(s.charAt(i));
+            int nowChar = romanCharToInt(s.charAt(i-1));
+            if(afterChar <= nowChar){
+                sum += nowChar;
+            }else if(afterChar > nowChar){
+                sum -= nowChar;
+            }
+        }
+        return sum;
+    }
+
+    public int romanCharToInt(char c){
+        switch (c){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
+    }
+}
